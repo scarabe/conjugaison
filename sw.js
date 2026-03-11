4792977c3888a39f389442517dd04a983b22adae
scarabe <scarabe@mac.com>
2026-03-11 18:45:13 +0100

    maj1
    mjlj

--- sw.js
+++ sw.js
@@ -1,8 +1,15 @@
-const CACHE = 'conjugo-v1';
+const CACHE = 'conjugo-v2';
 const ASSETS = ['/', '/index.html', '/manifest.json', '/icon-192.png', '/icon-512.png'];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys => Promise.all(
+    keys.filter(k => k !== CACHE).map(k => caches.delete(k))
+  )));
 });
 
 self.addEventListener('fetch', e => {
